d2b81b939b2572a8d90b9aef09e73b0202780880
Gianni Adamo <gianni.adamo@hotmail.it>
2023-02-04 01:10:44 +0100

    Add files via upload

---
+++ LabManagerDB/Database.sql
@@ -0,0 +1,469 @@
+-- tecnico
+CREATE TABLE tecnico
+(
+    matricola varchar(9) PRIMARY KEY,
+    pass varchar(16) UNIQUE,
+    nome varchar(20) NOT NULL,
+    cognome varchar(20) NOT NULL,
+    datanascita date NOT NULL,
+    sesso character(1) NOT NULL,
+    codicefiscale varchar(16) UNIQUE,
+    telefono varchar(10) UNIQUE,
+    email varchar(30) UNIQUE,
+	
+	CONSTRAINT matricolaLength CHECK(char_length(matricola) = 9),
+	CONSTRAINT passLength CHECK(char_length(pass) > 7),
+	CONSTRAINT CFLength CHECK(char_length(codicefiscale) = 16)
+);
+
+
+-- laboratorio
+CREATE TABLE laboratorio
+(
+	tipo varchar(20) PRIMARY KEY,
+	descrizione text NOT NULL,
+	numeroSedi integer DEFAULT 0
+);
+
+
+-- sede
+CREATE TABLE sede
+(
+	codSede serial PRIMARY KEY,
+	nome varchar(3) UNIQUE,
+	indirizzo varchar(30) NOT NULL
+);
+
+
+-- lavora
+CREATE TABLE lavora
+(
+	matricolaTecnico varchar(9) NOT NULL,
+	codSede integer NOT NULL,
+	tipo varchar(20) NOT NULL,
+	
+	CONSTRAINT matrTec FOREIGN KEY(matricolaTecnico) REFERENCES tecnico(matricola) ON DELETE CASCADE,
+	CONSTRAINT codiceSede FOREIGN KEY(codSede) REFERENCES sede(codSede) ON DELETE CASCADE,
+	CONSTRAINT tipoLaboratorio FOREIGN KEY(tipo) REFERENCES laboratorio(tipo) ON DELETE CASCADE
+);
+
+
+-- locazione
+CREATE TABLE locazione
+(
+	TLaboratorio varchar(20) NOT NULL,
+	Csede integer NOT NULL,
+	numeroTecnici integer DEFAULT 0,
+	numeroPostazioni integer DEFAULT 0,
+	responsabile varchar(9) DEFAULT NULL,
+	
+	CONSTRAINT onLaboratorio FOREIGN KEY(TLaboratorio) REFERENCES laboratorio(tipo) ON DELETE CASCADE,
+	CONSTRAINT onSede FOREIGN KEY(Csede) REFERENCES sede(codSede) ON DELETE CASCADE,
+	CONSTRAINT matricolaResponsabile FOREIGN KEY(responsabile) REFERENCES tecnico(matricola) ON UPDATE CASCADE
+);
+
+
+-- postazione
+CREATE TABLE postazione
+(
+	codPostazione serial PRIMARY KEY,
+	laboratorioPostazione varchar(20) NOT NULL,
+	sedePostazione integer NOT NULL,
+	maxStrumenti integer NOT NULL,
+	currentStrumenti integer NOT NULL DEFAULT 0,
+	
+	CONSTRAINT numeroStrumenti CHECK(currentStrumenti <= maxStrumenti)
+);
+
+
+-- strumento
+CREATE TABLE strumento
+(
+	codStrumento serial PRIMARY KEY,
+	tipo VARCHAR(20) NOT NULL,
+	descrizioneStrumento text NOT NULL,
+	utilizzoMax integer NOT NULL,
+	postazioneAssegnata integer NOT NULL,
+	
+	CONSTRAINT postazioneStrumento FOREIGN KEY(postazioneAssegnata) REFERENCES postazione(codPostazione) ON DELETE CASCADE
+);
+
+
+-- storico
+CREATE TABLE storico
+(
+	codicePrenotazione integer NOT NULL,
+	strumentoStorico integer NOT NULL,
+	utilizzatore VARCHAR(9) NOT NULL,
+	dataStorico date NOT NULL,
+	tempoUtilizzo integer NOT NULL,
+	
+	CONSTRAINT onStrumento FOREIGN KEY(strumentoStorico) REFERENCES strumento(codStrumento) ON DELETE CASCADE
+);
+
+
+-- prenotazioni
+CREATE TABLE prenotazioni
+(
+	codicePrenotazione serial PRIMARY KEY,
+	matricolaPrenotata varchar(9) NOT NULL,
+	strumentoPrenotato integer NOT NULL,
+	postazionePrenotata integer NOT NULL,
+	dataPrenotazione date NOT NULL,
+	orePrenotate integer NOT NULL,
+	conclusa boolean DEFAULT false,
+	
+	CONSTRAINT onMatricola FOREIGN KEY(matricolaPrenotata) REFERENCES tecnico(matricola) ON DELETE CASCADE,
+	CONSTRAINT onStrumentoPrenotato FOREIGN KEY(strumentoPrenotato) REFERENCES strumento(codStrumento) ON DELETE CASCADE
+	
+);
+---------------------------------------------------------------
+
+
+-- VIEW 1: postazione & locazione
+CREATE VIEW postazioneLocazione
+AS
+(
+SELECT pos.codpostazione,
+    pos.laboratoriopostazione,
+    pos.sedepostazione,
+    u.nome
+   FROM postazione pos
+     JOIN ( SELECT l.tlaboratorio,
+            l.csede,
+            s.nome
+           FROM locazione l
+             JOIN sede s ON l.csede = s.codsede) u ON pos.laboratoriopostazione::text = u.tlaboratorio::text AND pos.sedepostazione = u.csede;
+);
+
+
+-- VIEW 2: strumento & postazione
+CREATE VIEW strumentoCompleto
+AS
+(
+SELECT s.codstrumento,
+    s.tipo,
+    s.postazioneassegnata,
+	s.utilizzoMax,
+    cp.laboratoriopostazione,
+    cp.nome
+   FROM strumento s
+     JOIN postazionelocazione cp ON s.postazioneassegnata = cp.codpostazione;
+);
+---------------------------------------------------------------
+
+
+-- TRIGGER AUMENTO DEL NUMERO DI SEDI
+Create function numero_righe()  
+returns trigger
+language plpgsql
+AS  
+$$  
+Declare  
+risultato integer;
+materia varchar(20); 
+cur_righe cursor for select tipo  from laboratorio;
+Begin  
+	open cur_righe;
+	loop
+		fetch cur_righe into materia;
+		exit when not found;
+		select count(csede)
+		into risultato
+		from locazione
+		where tlaboratorio=materia;
+
+		update laboratorio set numerosedi= @risultato where tipo= materia;
+	end loop;
+	close cur_righe;	
+	return NEW;
+End;  
+$$;  
+
+CREATE TRIGGER aggiorna_numero_sedi
+    AFTER INSERT ON locazione
+    FOR EACH ROW
+    EXECUTE function numero_righe();
+	
+	
+
+-- TRIGGER AUMENTO DEL NUMERO DI TECNICI
+Create function numero_tecnici() 
+returns trigger
+language plpgsql
+AS  
+$$  
+Declare  
+risultato integer;
+materia varchar(20);
+codice_sede integer;
+cur_dati cursor for select csede,tlaboratorio from locazione;
+Begin  
+	open cur_dati;
+	risultato:=0;
+	loop
+	
+		fetch cur_dati into codice_sede,materia;
+		exit when not found;		
+		
+		select count(lavora.matricolatecnico)
+		into risultato
+		from lavora
+	    right join sede on lavora.codsede=sede.codSede
+		right join tecnico on lavora.matricolatecnico=tecnico.matricola
+		where lavora.codsede=codice_sede and lavora.tipo=materia;
+		
+		update locazione set numerotecnici=risultato where csede=codice_sede and tlaboratorio=materia;
+	end loop;
+	close cur_dati;
+	return NEW;
+End;  
+$$;  
+
+CREATE TRIGGER aggiorna_numero_tecnici
+    AFTER INSERT OR DELETE ON lavora
+    FOR EACH ROW
+    EXECUTE function numero_tecnici();
+	
+	
+	
+-- TRIGGER AUMENTO DEL NUMERO DI POSTAZIONI
+Create function numero_postazioni()  
+returns trigger
+language plpgsql
+AS  
+$$  
+Declare  
+ codice_laboratorio varchar(20);
+ codic_sede integer;
+ risultato integer; 
+ cur_dati cursor for select tlaboratorio,csede from locazione;
+Begin  
+	open cur_dati;
+	loop
+		fetch cur_dati into codice_laboratorio,codic_sede;
+		exit when not found;
+		
+		select count(codpostazione)
+		into risultato
+		from postazione
+		right join locazione 
+		on locazione.tlaboratorio=postazione.laboratoriopostazione and locazione.csede=postazione.sedepostazione
+		where locazione.tlaboratorio=codice_laboratorio and locazione.csede=codic_sede;
+		
+		update locazione set numeropostazioni=risultato where tlaboratorio=codice_laboratorio and csede=codic_sede;
+		
+	end loop;
+	close cur_dati;
+	return NEW;
+End;  
+$$; 
+
+CREATE TRIGGER aggiorna_numero_postazioni
+    AFTER INSERT OR DELETE ON postazione
+    FOR EACH ROW
+    EXECUTE function numero_postazioni();
+	
+	
+
+-- TRIGGER AUMENTO DEL NUMERO DI STRUMENTI
+Create function check_num_strumenti()  
+returns trigger
+language plpgsql
+AS  
+$$  
+Declare  
+cur_max_numero_strumenti cursor for select codpostazione,maxstrumenti from postazione;
+numero_max integer;
+codice integer;
+risultato integer;
+eccesso integer;
+BEGIN
+	open cur_max_numero_strumenti;
+	LOOP
+		FETCH cur_max_numero_strumenti into codice,numero_max;
+		EXIT WHEN NOT FOUND;
+
+		SELECT COUNT(codstrumento)
+		INTO risultato
+		FROM strumento
+		RIGHT JOIN postazione ON postazione.codpostazione=strumento.postazioneassegnata
+		WHERE postazioneassegnata=codice;
+
+		IF risultato<=numero_max THEN
+			UPDATE postazione SET currentstrumenti=risultato where codpostazione=codice;
+		ELSE	
+			
+			SELECT  LAST_VALUE(codstrumento) 
+				OVER(ORDER BY postazioneassegnata RANGE BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) ultimostrumento
+			into eccesso
+			FROM strumento
+			where postazioneassegnata=codice LIMIT 1;
+
+			delete from strumento where codstrumento=eccesso;
+			
+		END IF;
+		
+	END LOOP;
+	close cur_max_numero_strumenti;
+	return NEW;
+End;  
+$$; 
+
+CREATE TRIGGER aggiorna_numero_strumenti
+    AFTER INSERT OR DELETE ON strumento
+    FOR EACH ROW
+    EXECUTE function check_num_strumenti();
+	
+	
+
+-- TRIGGER SUL CONTROLLO DATA DISPONIBILE
+Create function check_prenotazione() 
+returns trigger
+language plpgsql
+AS  
+$$  
+Declare  
+DECLARE
+caso BOOLEAN;
+BEGIN
+	select case when not exists
+	(
+		select strumentoprenotato,postazioneprenotata,dataprenotazione
+		from prenotazioni where
+		strumentoprenotato=NEW.strumentoprenotato and postazioneprenotata=NEW.postazioneprenotata and
+		dataprenotazione=NEW.dataprenotazione
+	)
+	THEN TRUE 
+	ELSE FALSE 
+	into caso
+	END;
+	
+	IF caso=FALSE THEN
+		delete from prenotazioni where strumentoprenotato=NEW.strumentoprenotato and postazioneprenotata=NEW.postazioneprenotata and
+		dataprenotazione=NEW.dataprenotazione;
+		raise EXCEPTION 'strumento già prenotato in quella data!';
+	END IF;
+
+	
+	return NEW;
+End;  
+$$;  
+
+CREATE TRIGGER check_prenotazione_esistente
+    BEFORE INSERT ON prenotazioni
+    FOR EACH ROW
+    EXECUTE function check_prenotazione();
+	
+	
+	
+-- TRIGGER SUL CONTROLLO DELL ORE PRENOTATE
+Create function controllo_ore() 
+returns trigger
+language plpgsql
+AS  
+$$  
+DECLARE
+cursore_prenotazioni_strumento cursor for 
+	select prenotazioni.strumentoprenotato,prenotazioni.oreprenotate,strumento.utilizzoMax 
+	from prenotazioni,strumento 
+	where prenotazioni.strumentoprenotato=strumento.codstrumento;
+
+codice_struemento integer;
+orario1 integer;
+orario2 integer;
+
+BEGIN
+	open cursore_prenotazioni_strumento;
+
+	loop;
+		fetch cursore_prenotazioni_strumento into codice_struemento,orario1,orario2;
+		exit when not found;
+		
+		IF orario1>orario2 THEN
+			delete from prenotazioni where strumentoprenotato= codice_struemento and oreprenotate= orario1;
+		end if;
+		
+	end loop;
+	
+	close cursore_prenotazioni_strumento;
+
+	return NEW;
+End;  
+$$;  
+
+CREATE TRIGGER check_ore_utilizzo
+    AFTER INSERT ON prenotazioni
+    FOR EACH ROW
+    EXECUTE function controllo_ore();
+
+
+
+-- TRIGGER PASSAGGIO DA PRENOTAZIONE A STORICO
+Create function passaggio_dati() 
+returns trigger
+language plpgsql
+AS  
+$$  
+DECLARE
+codiceprenotato integer;
+codicestrumento integer;
+utente_tecnico varchar(9);
+data_storico date;
+tempo_utilizzo integer;
+
+cur_righe cursor FOR
+SELECT prenotazioni.codiceprenotazione, prenotazioni.matricolaprenotata, prenotazioni.strumentoprenotato,
+	   prenotazioni.dataprenotazione,prenotazioni.oreprenotate FROM prenotazioni WHERE prenotazioni.conclusa=TRUE;
+
+BEGIN
+	OPEN cur_righe;
+	LOOP
+		FETCH cur_righe into codiceprenotato,utente_tecnico,codicestrumento, data_storico, tempo_utilizzo;
+		EXIT WHEN NOT FOUND;
+		INSERT INTO storico (codiceprenotazione,utilizzatore,strumentostorico,datastorico,tempoutilizzo) 
+		VALUES(codiceprenotato,utente_tecnico,codicestrumento, data_storico, tempo_utilizzo);
+
+	END LOOP;
+	CLOSE cur_righe;
+	DELETE FROM prenotazioni where conclusa=TRUE;
+	return NEW;
+END;  
+$$;  
+
+CREATE TRIGGER salvataggio_dati_su_storico
+    AFTER INSERT OR UPDATE ON prenotazioni
+    FOR EACH ROW
+    EXECUTE function passaggio_dati();
+	
+---------------------------------------------------------------
+	
+	
+-- PROCEDURE TEMPO TRASCORSO
+CREATE PROCEDURE AGGIORNA_CONCLUSA()
+LANGUAGE PLPGSQL
+AS $$
+DECLARE 
+righe_prenotazioni cursor for select codiceprenotazione,dataprenotazione from prenotazioni;
+data_odierna date;
+
+codiceprenota integer;
+data_prenotata DATE;
+BEGIN
+	SELECT CAST(NOW()::timestamp AS DATE) INTO data_odierna;
+	OPEN righe_prenotazioni;
+	LOOP
+		FETCH righe_prenotazioni into codiceprenota,data_prenotata;
+		EXIT WHEN NOT FOUND;
+		
+		IF data_prenotata<data_odierna THEN
+		UPDATE prenotazioni SET conclusa=TRUE WHERE codiceprenotazione=codiceprenota;
+		END IF;
+		
+	END LOOP;
+	CLOSE righe_prenotazioni;
+	COMMIT;
+END;
+$$;
+
+---------------------------------------------------------------
---
+++ LabManagerDB/PopulationExample.sql
@@ -0,0 +1,143 @@
+-- popolamento laboratoro
+INSERT INTO laboratorio(tipo, descrizione) VALUES('Informatica', 'Sviluppo software, Cybersecurity, Studio di IA');
+INSERT INTO laboratorio(tipo, descrizione) VALUES('Chimica', 'Chimica organica, Chimica inorganica, Studio dei reagenti');
+INSERT INTO laboratorio(tipo, descrizione) VALUES('Biologia', 'Studio della cellula, Dissezione e analisi organiche');
+INSERT INTO laboratorio(tipo, descrizione) VALUES('Astronomia', 'Analisi dei corpi celesti');
+INSERT INTO laboratorio(tipo, descrizione) VALUES('Geologia', 'Studio di minerali, Vulcanologia, Fenomeni metereologici');
+INSERT INTO laboratorio(tipo, descrizione) VALUES('Fisica', 'Dinamica, Termodinamica, Leggi del moto, Temperature');
+
+
+-- popolamento sede
+INSERT INTO sede(nome, indirizzo) VALUES('MSA', 'Via Vicinale Cupa Cintia, 26'); --1
+INSERT INTO sede(nome, indirizzo) VALUES('VCL', 'Via Claudio, 21'); --2
+INSERT INTO sede(nome, indirizzo) VALUES('PZT', 'Piazzale Vincenzio Tecchio, 80'); --3
+INSERT INTO sede(nome, indirizzo) VALUES('GGC', 'Via Adriano, 83'); --4
+INSERT INTO sede(nome, indirizzo) VALUES('DCJ', 'Via Cornelia dei Gracchi, 45'); --5
+INSERT INTO sede(nome, indirizzo) VALUES('PRS', 'Via Canonico Scherillo, 18'); --6
+
+
+-- popolamento locazione
+INSERT INTO locazione(tlaboratorio, csede) VALUES('Informatica', 1);
+INSERT INTO locazione(tlaboratorio, csede) VALUES('Informatica', 2);
+INSERT INTO locazione(tlaboratorio, csede) VALUES('Informatica', 3);
+INSERT INTO locazione(tlaboratorio, csede) VALUES('Chimica', 3);
+INSERT INTO locazione(tlaboratorio, csede) VALUES('Chimica', 2);
+INSERT INTO locazione(tlaboratorio, csede) VALUES('Biologia', 1);
+INSERT INTO locazione(tlaboratorio, csede) VALUES('Biologia', 3);
+INSERT INTO locazione(tlaboratorio, csede) VALUES('Biologia', 4);
+INSERT INTO locazione(tlaboratorio, csede) VALUES('Astronomia', 2);
+INSERT INTO locazione(tlaboratorio, csede) VALUES('Astronomia', 3);
+INSERT INTO locazione(tlaboratorio, csede) VALUES('Informatica', 4);
+INSERT INTO locazione(tlaboratorio, csede) VALUES('Astronomia', 1);
+INSERT INTO locazione(tlaboratorio, csede) VALUES('Geologia', 2);
+INSERT INTO locazione(tlaboratorio, csede) VALUES('Geologia', 4);
+INSERT INTO locazione(tlaboratorio, csede) VALUES('Fisica', 5);
+INSERT INTO locazione(tlaboratorio, csede) VALUES('Fisica', 6);
+INSERT INTO locazione(tlaboratorio, csede) VALUES('Informatica', 6);
+INSERT INTO locazione(tlaboratorio, csede) VALUES('Chimica', 5);
+
+
+-- popolamento tecnico
+INSERT INTO tecnico(matricola, pass, nome, cognome, datanascita, sesso, codicefiscale, telefono, email)
+VALUES('LM1990401', 'GianniLab', 'Giovanni', 'Adamo', '2001-05-27', 'M', 'DMAGNN01E27F839H', '3469773472', 'gianni.adamo@hotmail.it');
+
+INSERT INTO tecnico(matricola, pass, nome, cognome, datanascita, sesso, codicefiscale, telefono, email)
+VALUES('LM1633608', 'Jacopixel33', 'Jacopo', 'de Leo', '2001-09-17', 'M', 'DLEJCP01P17F839X', '3921821391', 'jacopo.deleo@hotmail.it');
+
+INSERT INTO tecnico(matricola, pass, nome, cognome, datanascita, sesso, codicefiscale, telefono, email)
+VALUES('LM1493704', 'SteveGames88', 'Fabio', 'Sarà', '2001-11-04', 'M', 'SRAFBA01S04F839C', '3935397033', 'fabiosara@hotmail.it');
+
+INSERT INTO tecnico(matricola, pass, nome, cognome, datanascita, sesso, codicefiscale, telefono, email)
+VALUES('LM1391596', 'PassChaddone', 'Carlo', 'De Angelis', '2001-07-18', 'M', 'DNGCRL01L18F839A', '3421800844', 'carlodeangelis@hotmail.it');
+
+INSERT INTO tecnico (matricola, pass, nome, cognome, datanascita, sesso, codicefiscale, telefono, email) 
+VALUES ('LM1234567', 'passMarioR', 'Mario', 'Rossi', '1990-03-15', 'M', 'RSSMRA90A01H501Q', '1234567890', 'mario.rossi@email.com');
+
+INSERT INTO tecnico (matricola, pass, nome, cognome, datanascita, sesso, codicefiscale, telefono, email) 
+VALUES ('LM8765432', 'passLuciaB', 'Lucia', 'Bianchi', '1995-02-14', 'F', 'BNCLCU95B54G702A', '9876543210', 'lucia.bianchi@email.com');
+
+INSERT INTO tecnico (matricola, pass, nome, cognome, datanascita, sesso. codicefiscale, telefono, email)
+VALUES ('LM7842105', 'passDavide11', 'Davide', 'Crispano', '2001-06-07', 'M', 'CRSDVD01E07F839A', '3286543210', 'davide.crispano@gmail.com');
+
+
+-- popolamento lavora
+INSERT INTO lavora(matricolatecnico, codsede, tipo) VALUES('LM1990401', 1, 'Informatica');
+INSERT INTO lavora(matricolatecnico, codsede, tipo) VALUES('LM1633608',3,'Chimica');
+INSERT INTO lavora(matricolatecnico, codsede, tipo) VALUES('LM1493704',1,'Biologia');
+INSERT INTO lavora(matricolatecnico, codsede, tipo) VALUES('LM1633608',3,'Astronomia');
+INSERT INTO lavora(matricolatecnico, codsede, tipo) VALUES('LM1391596',2,'Geologia');
+INSERT INTO lavora(matricolatecnico, codsede, tipo) VALUES('LM1234567',5,'Fisica');
+INSERT INTO lavora(matricolatecnico, codsede, tipo) VALUES('LM8765432',6,'Fisica');
+INSERT INTO lavora(matricolatecnico, codsede, tipo) VALUES('LM7842105',4,'Geologia');
+INSERT INTO lavora(matricolatecnico, codsede, tipo) VALUES('LM1391596',2,'Informatica');
+INSERT INTO lavora(matricolatecnico, codsede, tipo) VALUES('LM8765432',4,'Informatica');
+INSERT INTO lavora(matricolatecnico, codsede, tipo) VALUES('LM7842105',2,'Chimica');
+INSERT INTO lavora(matricolatecnico, codsede, tipo) VALUES('LM1990401',1,'Astronomia');
+INSERT INTO lavora(matricolatecnico, codsede, tipo) VALUES('LM1633608',2,'Geologia');
+INSERT INTO lavora(matricolatecnico, codsede, tipo) VALUES('LM1391596',3,'Biologia');
+INSERT INTO lavora(matricolatecnico, codsede, tipo) VALUES('LM7842105',6,'Informatica');
+INSERT INTO lavora(matricolatecnico, codsede, tipo) VALUES('LM8765432',5,'Fisica');
+INSERT INTO lavora(matricolatecnico, codsede, tipo) VALUES('LM1391596',5,'Chimica');
+INSERT INTO lavora(matricolatecnico, codsede, tipo) VALUES('LM8765432',4,'Geologia');
+
+
+-- popolamento postazione
+INSERT INTO postazione(codpostazione, laboratoriopostazione, sedepostazione, maxstrumenti, currentstrumenti) VALUES(1,'Informatica',1,2,0);
+INSERT INTO postazione(codpostazione, laboratoriopostazione, sedepostazione, maxstrumenti, currentstrumenti) VALUES(2,'Chimica',3,3,0);
+INSERT INTO postazione(codpostazione, laboratoriopostazione, sedepostazione, maxstrumenti, currentstrumenti) VALUES(3,'Biologia',1,2,0);
+INSERT INTO postazione(codpostazione, laboratoriopostazione, sedepostazione, maxstrumenti, currentstrumenti) VALUES(4,'Atronomia',3,3,0);
+INSERT INTO postazione(codpostazione, laboratoriopostazione, sedepostazione, maxstrumenti, currentstrumenti) VALUES(5,'Geologia',2,4,0);
+INSERT INTO postazione(codpostazione, laboratoriopostazione, sedepostazione, maxstrumenti, currentstrumenti) VALUES(6,'Fisica',5,2,0);
+INSERT INTO postazione(codpostazione, laboratoriopostazione, sedepostazione, maxstrumenti, currentstrumenti) VALUES(7,'Fisica',6,3,0);
+INSERT INTO postazione(codpostazione, laboratoriopostazione, sedepostazione, maxstrumenti, currentstrumenti) VALUES(8,'Geologia',4,3,0);
+INSERT INTO postazione(codpostazione, laboratoriopostazione, sedepostazione, maxstrumenti, currentstrumenti) VALUES(9,'Informatica',2,4,0);
+INSERT INTO postazione(codpostazione, laboratoriopostazione, sedepostazione, maxstrumenti, currentstrumenti) VALUES(10,'Informatica',4,6,0);
+INSERT INTO postazione(codpostazione, laboratoriopostazione, sedepostazione, maxstrumenti, currentstrumenti) VALUES(11,'Chimica',2,2,0);
+INSERT INTO postazione(codpostazione, laboratoriopostazione, sedepostazione, maxstrumenti, currentstrumenti) VALUES(12,'Astronomia',1,3,0);
+INSERT INTO postazione(codpostazione, laboratoriopostazione, sedepostazione, maxstrumenti, currentstrumenti) VALUES(13,'Geologia',2,3,0);
+INSERT INTO postazione(codpostazione, laboratoriopostazione, sedepostazione, maxstrumenti, currentstrumenti) VALUES(14,'Biologia',3,4,0);
+INSERT INTO postazione(codpostazione, laboratoriopostazione, sedepostazione, maxstrumenti, currentstrumenti) VALUES(15,'Informatica',6,2,0);
+INSERT INTO postazione(codpostazione, laboratoriopostazione, sedepostazione, maxstrumenti, currentstrumenti) VALUES(16,'Fisica',5,3,0);
+INSERT INTO postazione(codpostazione, laboratoriopostazione, sedepostazione, maxstrumenti, currentstrumenti) VALUES(17,'Chimica',5,3,0);
+INSERT INTO postazione(codpostazione, laboratoriopostazione, sedepostazione, maxstrumenti, currentstrumenti) VALUES(18,'Geologia',4,4,0);
+
+
+-- popolamento prenotazioni
+INSERT INTO prenotazioni(codiceprenotazione ,matricolaprenotata, strumentoprenotato, postazioneprenotata, dataprenotazione, oreprenotate, conclusa) VALUES(1,'LM1990401',1,1,'2023-02-03',2,false);
+INSERT INTO prenotazioni(codiceprenotazione ,matricolaprenotata, strumentoprenotato, postazioneprenotata, dataprenotazione, oreprenotate, conclusa) VALUES(2,'LM1633608',2,9,'2023-03-24',1,false);
+INSERT INTO prenotazioni(codiceprenotazione ,matricolaprenotata, strumentoprenotato, postazioneprenotata, dataprenotazione, oreprenotate, conclusa) VALUES(3,'LM1493704',3,10,'2023-04-23',1,false);
+INSERT INTO prenotazioni(codiceprenotazione ,matricolaprenotata, strumentoprenotato, postazioneprenotata, dataprenotazione, oreprenotate, conclusa) VALUES(4,'LM1391596',4,15,'2023-06-20',2,false);
+INSERT INTO prenotazioni(codiceprenotazione ,matricolaprenotata, strumentoprenotato, postazioneprenotata, dataprenotazione, oreprenotate, conclusa) VALUES(5,'LM1234567',5,2,'2023-08-18',3,false);
+INSERT INTO prenotazioni(codiceprenotazione ,matricolaprenotata, strumentoprenotato, postazioneprenotata, dataprenotazione, oreprenotate, conclusa) VALUES(6,'LM8765432',6,11,'2023-11-08',2,false);
+INSERT INTO prenotazioni(codiceprenotazione ,matricolaprenotata, strumentoprenotato, postazioneprenotata, dataprenotazione, oreprenotate, conclusa) VALUES(7,'LM7842105',7,17,'2023-12-05',1,false);
+INSERT INTO prenotazioni(codiceprenotazione ,matricolaprenotata, strumentoprenotato, postazioneprenotata, dataprenotazione, oreprenotate, conclusa) VALUES(8,'LM8765432',8,3,'2023-05-03',1,false);
+INSERT INTO prenotazioni(codiceprenotazione ,matricolaprenotata, strumentoprenotato, postazioneprenotata, dataprenotazione, oreprenotate, conclusa) VALUES(9,'LM1990401',9,14,'2023-08-21',2,false);
+INSERT INTO prenotazioni(codiceprenotazione ,matricolaprenotata, strumentoprenotato, postazioneprenotata, dataprenotazione, oreprenotate, conclusa) VALUES(10,'LM1990401',1,3,'2023-09-13',3,false);
+
+
+-- popolamento strumento
+INSERT INTO strumento(codstrumento, tipo, descrizionestrumento, utilizzomax, postazioneassegnata) VALUES(1,'Computer Fisso','Computer marca DELL',2,1);
+INSERT INTO strumento(codstrumento, tipo, descrizionestrumento, utilizzomax, postazioneassegnata) VALUES(2,'Stampante Laser','Stampante marca EPSON',3,9);
+INSERT INTO strumento(codstrumento, tipo, descrizionestrumento, utilizzomax, postazioneassegnata) VALUES(3,'Plotter','Plotter marca HP',1,10);
+INSERT INTO strumento(codstrumento, tipo, descrizionestrumento, utilizzomax, postazioneassegnata) VALUES(4,'MainFrame','MainFrame marca IBM',2,15);
+INSERT INTO strumento(codstrumento, tipo, descrizionestrumento, utilizzomax, postazioneassegnata) VALUES(5,'Becco Bunsen','Marca Neolab',3,2);
+INSERT INTO strumento(codstrumento, tipo, descrizionestrumento, utilizzomax, postazioneassegnata) VALUES(6,'Refrigeranti','Marca Allihn',4,11);
+INSERT INTO strumento(codstrumento, tipo, descrizionestrumento, utilizzomax, postazioneassegnata) VALUES(7,'Beuta','Beuta Erlenmeyer collo largo',2,17);
+INSERT INTO strumento(codstrumento, tipo, descrizionestrumento, utilizzomax, postazioneassegnata) VALUES(8,'Microscopio','Marca Digiteck',2,3);
+INSERT INTO strumento(codstrumento, tipo, descrizionestrumento, utilizzomax, postazioneassegnata) VALUES(9,'Pallone','Pallone per colture cellulari',3,14);
+INSERT INTO strumento(codstrumento, tipo, descrizionestrumento, utilizzomax, postazioneassegnata) VALUES(10,'Imbuto','Imbuto in plastica',4,3);
+INSERT INTO strumento(codstrumento, tipo, descrizionestrumento, utilizzomax, postazioneassegnata) VALUES(11,'Vetrini','Vetrini per microscopio',2,14);
+INSERT INTO strumento(codstrumento, tipo, descrizionestrumento, utilizzomax, postazioneassegnata) VALUES(12,'Kit dissezione','Marca Ronaldo',2,14);
+INSERT INTO strumento(codstrumento, tipo, descrizionestrumento, utilizzomax, postazioneassegnata) VALUES(13,'Planetario','Planetario portatile SPLN',3,12);
+INSERT INTO strumento(codstrumento, tipo, descrizionestrumento, utilizzomax, postazioneassegnata) VALUES(14,'Telescopio','Telescopio multi accessoriato',1,12);
+INSERT INTO strumento(codstrumento, tipo, descrizionestrumento, utilizzomax, postazioneassegnata) VALUES(15,'Telescopio','Marca Taurus',2,12);
+INSERT INTO strumento(codstrumento, tipo, descrizionestrumento, utilizzomax, postazioneassegnata) VALUES(16,'Bussola','Marca DQY',3,5);
+INSERT INTO strumento(codstrumento, tipo, descrizionestrumento, utilizzomax, postazioneassegnata) VALUES(17,'Sismografo','Marca WitMotion',4,8);
+INSERT INTO strumento(codstrumento, tipo, descrizionestrumento, utilizzomax, postazioneassegnata) VALUES(18,'Geofono sismico','Marca Set',1,13);
+INSERT INTO strumento(codstrumento, tipo, descrizionestrumento, utilizzomax, postazioneassegnata) VALUES(19,'Dinamometro','Marca Sauter',3,7);
+INSERT INTO strumento(codstrumento, tipo, descrizionestrumento, utilizzomax, postazioneassegnata) VALUES(20,'Peseria','Class certificata Accredia',4,6);
+INSERT INTO strumento(codstrumento, tipo, descrizionestrumento, utilizzomax, postazioneassegnata) VALUES(21,'Torsiometro','Marca Metrica',1,16);
+
+
+-- popolamento storico
+-- (vuoto, viene inserito automaticamente tramite la procedure aggiorna_conclusa)
